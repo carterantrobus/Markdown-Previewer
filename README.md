8c72f9c53e344103c29121304fbcbb1ab69a40db
Carter Antrobus <carter.antrobus@ontariotechu.net>
2023-08-03 19:40:54 -0400

    Adding README

---
+++ README.md
@@ -0,0 +1,12 @@
+# Markdown Previewer
+### Welcome to my Markdown Previewer source code!
+
+
+
+#### This project was created with:
+
+* React
+* JQeury
+* Styled Components
+
+It contains two components, the editor and the previewer, which can both be maximized to focus on one or the other.
--- script.js
+++ script.js
@@ -161,7 +161,7 @@ var c = function(t) {
           className: e[1]
         }, React.createElement(l, {
           icon: e[2],
-          onClick: this.handlPreviewMaximize,
+          onClick: this.handlePreviewMaximize,
           text: "Previewer"
         }), React.createElement(d, {
           markdown: this.state.markdown
@@ -202,6 +202,6 @@ var c = function(t) {
           id : "preview"
         })
       }
-        , u = "# Welcome to my React Markdown Previewer!\n\n## This is a sub-heading...\n### And here's some other cool stuff:\n\nHeres some code, `<div></div>`, between 2 backticks. \n\n```\n// this is multi-line code:\n\nfunciton anotherExample(firstline, lastline) {\n  if (firstLine == '```' && lastLine == '```') {\n    return multiLineCode;\n  }\n}\n```\n\nYou can also make text **bold**... whoa!\nOr _italic_.\nOr... wait for it... **_both!_**\nAnd feel free to go crazy ~~crossing stuff out~~.\n\nThere's also [links](https://www.freecodecamp.org), and\n> Block Quotes!\n\nAnd if you want to get really crazy, even tables:\n\nWild Header | Crazy Header | Another Header?\n------------ | ------------- | -------------\nYour content can | be here, and it | can be here....\nAnd here. | Okay. | I think we get it.\n\n- And of course there are lists.\n  - Some are bulleted.\n     - With different indentation levels.\n        - That look like this.\n\n\n1. And there are numbered lists too.\n1. Use just 1s if you want!\n1. And last but not least, let's not forget embedded images:\n\n![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)\n";
+        , u = "# Welcome to my React Markdown Previewer!\n\n## This is a sub-heading...\n### And here's some other cool stuff:\n\nHeres some code, `<div></div>`, between 2 backticks. \n\n```\n// this is multi-line code:\n\nfunciton anotherExample(firstline, lastline) {\n  if (firstLine == '```' && lastLine == '```') {\n    return multiLineCode;\n  }\n}\n```\n\nYou can also make text **bold**... whoa!\nOr _italic_.\nOr... wait for it... **_both!_**\nAnd feel free to go crazy ~~crossing stuff out~~.\n\nThere's also [links](https://www.freecodecamp.org), and\n> Block Quotes!\n\nAnd if you want to get really crazy, even tables:\n\nWild Header | Crazy Header | Another Header?\n------------ | ------------- | -------------\nYour content can | be here, and it | can be here....\nAnd here. | Okay. | I think we get it.\n\n- And of course there are lists.\n  - Some are bulleted.\n     - With different indentation levels.\n        - That look like this.\n\n\n1. And there are numbered lists too.\n1. Use just 1s if you want!\n1. And last but not least, let's not forget embedded images:\n\n![Random Picture](./images/horse.png)\n";
         ReactDOM.render(React.createElement(c, null), document.getElementById("app"))    
 }();
